a5e8a0087ff5f692f10c852d8ac394d724823e10
Suraj Bhushan Pandey <sbpandey186@gmail.com>
2022-01-16 13:26:05 +0000

    Sync LeetCode submission - Binary Tree Preorder Traversal (javascript)

---
+++ problems/binary_tree_preorder_traversal/solution.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+   const _result = [];
+    
+    const _recursiveCalls = node => {
+        if (node) {
+            _result.push(node.val);
+            _recursiveCalls(node.left);
+            _recursiveCalls(node.right);
+        }
+    }
+    
+    _recursiveCalls(root);
+    
+    return _result;
+};
