efee6341b46a76204031453a65ab863f4de4f77f
Suraj Bhushan Pandey <sbpandey186@gmail.com>
2022-06-19 06:43:50 +0000

    Sync LeetCode submission - Single Number (java)

---
+++ problems/single_number/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int op = 0;
+        
+        for (int i : nums) {
+           op ^= i;   
+        }
+        
+        return op;
+    }
+}
